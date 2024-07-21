b295f23949db29f1959f36b2db6c106868fe0a57
Victor Adeleye <victoradeleye07@example.com>
2024-07-21 06:22:39 +0100

    BuyMe shopping project

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Hero from "./components/Hero";
+import NewProduct from "./components/NewProduct";
+import Testimonial from "./components/Testimonial";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <Hero />
+        <NewProduct />
+        <Testimonial />
+      </main>
+    </>
+  );
+}
